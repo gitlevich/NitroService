d5c248a495fddf1564281a70a5b79fd199d511ba
Vladimir Gitlevich <vgitlevich@gracenote.com>
2015-12-24 15:09:15 -0800

    Renamed the entry point

--- src/main/scala/com/gracenote/extraction/bbc/akka/Main.scala
+++ src/main/scala/com/gracenote/extraction/bbc/akka/Main.scala
@@ -10,20 +10,22 @@ import org.joda.time.DateTime
 import scala.concurrent.duration._
 import scala.io.Source
 
-object ActorBasedFetcher extends App {
+object Main extends App {
   require(args.length == 2, "Start and end dates required in format YYYY-MM-dd")
   val from = DateTime.parse(args(0))
   val to = DateTime.parse(args(1))
   require(from.isBefore(to), "Start date must be before end date")
+
+  val outputFileName = "schedules.csv"
   val serviceIds = Source.fromInputStream(getClass.getResourceAsStream("/providers.txt")).getLines()
 
-  val coordinator = new ActorBasedFetcher().startUpCoordinator(Rate(90, 1 second))
-  coordinator ! StartExtraction(new File("schedules.csv"), Rate(90, 1 second))
+  val coordinator = new Main().startUpCoordinator(Rate(90, 1 second))
+  coordinator ! StartExtraction(new File(outputFileName), Rate(90, 1 second))
 
   serviceIds map (id => ScheduleRequest(id, from, to)) foreach (request => coordinator ! request)
 }
 
-class ActorBasedFetcher {
+class Main {
   def startUpCoordinator(rate: Rate) =
     ActorSystem("Schedules").actorOf(Props(new Coordinator()))
 }
